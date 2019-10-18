035407d1eb9b76023d8bc0075ce0feca6f95a270
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-10-18 17:15:35 +0300

    Refactor dd() to pass arguments to dump() correctly

--- core/helpers.php
+++ core/helpers.php
@@ -14,7 +14,7 @@ if (!function_exists('dump')) {
 if (!function_exists('dd')) {
     function dd()
     {
-        dump(func_get_args());
+        dump(...func_get_args());
         die();
     }
 }
